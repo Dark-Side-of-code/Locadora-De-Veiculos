b6a2e5bc9e3dfcb8bacc374fd3477f824982bf26
Carlos <cdgamerbr007@gmail.com>
2022-06-22 16:11:51 -0300

    Inserção do banco de dados

---
+++ Locadora-De-Veiculos.Infra.SqlServer.Database/dbo/Tables/TbCliente.sql
@@ -0,0 +1,12 @@
+﻿CREATE TABLE [dbo].[TbCliente] (
+    [Id]           INT           IDENTITY (1, 1) NOT NULL,
+    [Nome]         VARCHAR (300) NOT NULL,
+    [CPF/CNPJ]     VARCHAR (50)  NOT NULL,
+    [CNH]          VARCHAR (50)  NOT NULL,
+    [Validade_CNH] DATETIME      NOT NULL,
+    [Tipo_cliente] VARCHAR (50)  NOT NULL,
+    [Email]        VARCHAR (200) NOT NULL,
+    [Telefone]     VARCHAR (20)  NOT NULL,
+    CONSTRAINT [PK_TbCliente] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
---
+++ Locadora-De-Veiculos.Infra.SqlServer.Database/dbo/Tables/TbTaxas.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[TbTaxas] (
+    [Id]          INT             IDENTITY (1, 1) NOT NULL,
+    [Nome]        VARCHAR (200)   NOT NULL,
+    [Descricao]   VARCHAR (500)   NOT NULL,
+    [Valor]       DECIMAL (18, 2) NOT NULL,
+    [Diario/Fixo] VARCHAR (50)    NOT NULL,
+    CONSTRAINT [PK_TbTaxas] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
