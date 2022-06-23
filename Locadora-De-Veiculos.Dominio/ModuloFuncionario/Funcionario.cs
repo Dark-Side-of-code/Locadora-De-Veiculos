42b44f9c827a782f7ab52a5510566d4af9392a22
PedroSRN <pedraoflamenguista@hotmail.com>
2022-06-23 18:55:31 -0300

    Alterações na classe de funcionario

--- Locadora-De-Veiculos.Dominio/ModuloFuncionario/Funcionario.cs
+++ Locadora-De-Veiculos.Dominio/ModuloFuncionario/Funcionario.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Locadora_De_Veiculos.Dominio.Compartilhado;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,39 @@ using System.Threading.Tasks;
 
 namespace Locadora_De_Veiculos.Dominio.ModuloFuncionario
 {
-    internal class Funcionario
+    public class Funcionario : EntidadeBase<Funcionario>
     {
+        public Funcionario()
+        {
+
+        }
+
+        public Funcionario(string nome, string login, string senha, double salario, DateTime dataAdmissao, bool tipoFuncionario )
+        {
+            Nome = nome;
+            Login = login;
+            Senha = senha;
+            Salario = salario;
+            DataAdmissao = dataAdmissao;
+            TipoFuncionario = tipoFuncionario;
+        }
+       public string Nome { get; set; }
+       public string Login { get; set; }
+       public string Senha { get; set; }
+       public double Salario { get; set; }
+       public DateTime DataAdmissao { get; set; }
+        public bool TipoFuncionario { get; set; }
+
+        public override bool Equals(object obj)
+        {
+            return obj is Funcionario funcionario &&
+                   Id == funcionario.Id &&
+                   Nome == funcionario.Nome &&
+                   Login == funcionario.Login &&
+                   Senha == funcionario.Senha &&
+                   Salario == funcionario.Salario &&
+                   DataAdmissao == funcionario.DataAdmissao &&
+                   TipoFuncionario == funcionario.TipoFuncionario;
+        }
     }
 }
--- Locadora-De-Veiculos.Infra.Banco/ModuloFuncionario/RepositorioFuncionarioEmBancoDados.cs
+++ Locadora-De-Veiculos.Infra.Banco/ModuloFuncionario/RepositorioFuncionarioEmBancoDados.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Locadora_De_Veiculos.Dominio.ModuloFuncionario;
+using Locadora_De_Veiculos.Infra.Banco.Compartilhado;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,70 @@ using System.Threading.Tasks;
 
 namespace Locadora_De_Veiculos.Infra.Banco.ModuloFuncionario
 {
-    internal class RepositorioFuncionarioEmBancoDados
+    public class RepositorioFuncionarioEmBancoDados : RepositorioBase<Funcionario, ValidadorFuncionario, MapeadorFuncionario>
     {
+        protected override string sqlInserir =>
+            @"INSERT INTO [TBFUNCIONARIO]
+            (
+		        [NOME],
+                [LOGIN],
+                [SENHA],
+                [SALARIO],
+                [DATA_ADMISSAO],
+                [TIPOFUNCIONARIO]
+		    )
+
+            VALUES
+            (
+		        @NOME,
+			    @LOGIN,
+			    @SENHA,
+			    @SALARIO,
+			    @DATA_ADMISSAO,
+			    @TIPOFUNCIONARIO
+		    );SELECT SCOPE_IDENTITY();";
+
+        protected override string sqlEditar =>
+            @"UPDATE [TBFUNCIONARIO]
+            SET
+	            [NOME] = @NOME,
+                [LOGIN] = @LOGIN,
+                [SENHA] = @SENHA,
+                [SALARIO] = @SALARIO,
+                [DATA_ADMISSAO] = @DATA_ADMISSAO,
+                [TIPOFUNCIONARIO] = @TIPOFUNCIONARIO
+	        WHERE
+	            ID = @ID;";
+
+        protected override string sqlExcluir =>
+            @"DELETE FROM [TBFUNCIONARIO]
+            WHERE
+	             ID = @ID;";
+
+        protected override string sqlSelecionarPorId =>
+            @"SELECT 
+                [ID],
+                [NOME],
+                [LOGIN],
+                [SENHA],
+                [SALARIO],
+                [DATA_ADMISSAO],
+                [TIPOFUNCIONARIO]
+            FROM
+                [TBFUNCIONARIO]
+            WHERE
+                 ID = @ID";
+
+        protected override string sqlSelecionarTodos =>
+            @"SELECT 
+                [ID],
+                [NOME],
+                [LOGIN],
+                [SENHA],
+                [SALARIO],
+                [DATA_ADMISSAO],
+                [TIPOFUNCIONARIO]
+            FROM
+                [TBFUNCIONARIO]";
     }
 }
