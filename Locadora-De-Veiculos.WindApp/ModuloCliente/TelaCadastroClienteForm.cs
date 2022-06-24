e05a73cbf8ac166ade0a85e87172cdfce2f97013
Carlos <cdgamerbr007@gmail.com>
2022-06-24 17:40:36 -0300

    correção de erros na apresentação no modul0o cliente

--- Locadora-De-Veiculos.WindApp/ModuloCliente/TelaCadastroClienteForm.cs
+++ Locadora-De-Veiculos.WindApp/ModuloCliente/TelaCadastroClienteForm.cs
@@ -29,25 +29,39 @@ namespace Locadora_De_Veiculos.WindApp.ModuloCliente
             {
                 cliente = value;
 
-                groupBoxTipoCliente.Text = cliente.Tipo_Cliente;
+                rb_F.Text = cliente.Tipo_Cliente;
+                rb_J.Text = cliente.Tipo_Cliente;
                 txtMask_Cpf_Cnpj.Text = cliente.CPF_CNPJ;
                 txt_Email.Text = cliente.Email;
                 txtMask_Fone.Text = cliente.Telefone;
                 txt_Nome.Text = cliente.Nome;
                 txtMask_Cnh.Text = cliente.CNH;
-                dateTime_Validade_Cnh.Text = cliente.Validade_CNH.ToString(); 
+                if(Cliente.Validade_CNH > DateTimePicker.MinimumDateTime)
+                {
+                    dateTime_Validade_Cnh.Value = cliente.Validade_CNH;
+                }else
+                    dateTime_Validade_Cnh.Value = DateTimePicker.MinimumDateTime;
+                 
             }
         }
 
         private void btn_Cadastrar_Click(object sender, EventArgs e)
         {
-            cliente.Tipo_Cliente = groupBoxTipoCliente.Text;
+            if (rb_F.Checked)
+            {
+                cliente.Tipo_Cliente = "Pessoa Física";
+            }
+            else
+            {
+                cliente.Tipo_Cliente = "Pessoa Júridica";
+            }
+
             cliente.CPF_CNPJ = txtMask_Cpf_Cnpj.Text;
             cliente.Email = txt_Email.Text;
             cliente.Telefone = txtMask_Fone.Text;
             cliente.Nome = txt_Nome.Text;
             cliente.CNH = txtMask_Cnh.Text;
-            cliente.Validade_CNH = Convert.ToDateTime(dateTime_Validade_Cnh);
+            cliente.Validade_CNH = DateTime.Parse(dateTime_Validade_Cnh.Text);
 
             var resultadoValidacao = GravarRegistro(Cliente);
 
