ba7f3abd7a4bec7faed8f7824e56afcbda34442d
Carlos <cdgamerbr007@gmail.com>
2022-06-30 18:26:15 -0300

    Atualizado banco de dados

---
+++ Locadora-De-Veiculos.Infra.SqlServer.Database/dbo/Tables/TbCondutor.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE [dbo].[TbCondutor] (
+    [Id]         INT           NOT NULL,
+    [Nome]       VARCHAR (300) NOT NULL,
+    [CPF]        VARCHAR (50)  NOT NULL,
+    [CNH]        VARCHAR (50)  NOT NULL,
+    [Validade]   DATETIME      NOT NULL,
+    [Email]      VARCHAR (200) NOT NULL,
+    [Telefone]   VARCHAR (20)  NOT NULL,
+    [Endereço]   VARCHAR (300) NOT NULL,
+    [Cliente_Id] INT           NOT NULL,
+    CONSTRAINT [PK_TbCondutor] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_TbCondutor_TbCliente] FOREIGN KEY ([Cliente_Id]) REFERENCES [dbo].[TbCliente] ([Id])
+);
+
---
+++ Locadora-De-Veiculos.Infra.SqlServer.Database/dbo/Tables/TbVeiculo.sql
@@ -0,0 +1,16 @@
+﻿CREATE TABLE [dbo].[TbVeiculo] (
+    [Id]                INT             NOT NULL,
+    [Modelo]            VARCHAR (300)   NOT NULL,
+    [Placa]             VARCHAR (7)     NOT NULL,
+    [Marca]             VARCHAR (50)    NOT NULL,
+    [Cor]               VARCHAR (100)   NOT NULL,
+    [Tipo_combustivel]  VARCHAR (100)   NOT NULL,
+    [Capacidade_tanque] DECIMAL (18, 2) NOT NULL,
+    [Ano]               DATETIME        NOT NULL,
+    [Km_total]          DECIMAL (18, 2) NOT NULL,
+    [Foto]              VARBINARY (MAX) NULL,
+    [Categoria_Id]      INT             NOT NULL,
+    CONSTRAINT [PK_TbVeiculo] PRIMARY KEY CLUSTERED ([Id] ASC),
+    CONSTRAINT [FK_TbVeiculo_TbCategoriaVeiculo] FOREIGN KEY ([Categoria_Id]) REFERENCES [dbo].[TbCategoriaVeiculo] ([Id])
+);
+
