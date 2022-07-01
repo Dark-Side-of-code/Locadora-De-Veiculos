d4436e9e9bad67bac0238364ed2c697288203d63
PedroSRN <pedraoflamenguista@hotmail.com>
2022-07-01 14:48:12 -0300

    Dominio Concluido

--- Locadora-De-Veiculos.Dominio/ModuloPlanosDeCobranca/ValidadorPlanoDeCobranca.cs
+++ Locadora-De-Veiculos.Dominio/ModuloPlanosDeCobranca/ValidadorPlanoDeCobranca.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FluentValidation;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,46 @@ using System.Threading.Tasks;
 
 namespace Locadora_De_Veiculos.Dominio.ModuloPlanosDeCobranca
 {
-    public class ValidadorPlanoDeCobranca
+    public class ValidadorPlanoDeCobranca : AbstractValidator<PlanoDeCobranca>
     {
+        public ValidadorPlanoDeCobranca()
+        {
+            RuleFor(x => x.CategoriaDeVeiculos)
+                .NotEmpty()
+                .NotNull();
+
+            RuleFor(x => x.PlanoDiario_ValorDiario)
+                .NotNull()
+                .NotEmpty()
+                .GreaterThan(0);
+
+            RuleFor(x => x.PlanoDiario_ValorPorKM)
+                .NotNull()
+                .NotEmpty()
+                .GreaterThan(0);
+
+            RuleFor(x => x.PlanoKM_Livre_ValorDiario)
+                .NotNull()
+                .NotEmpty()
+                .GreaterThan(0);
+
+            RuleFor(x => x.PlanoKM_controlado_LimiteDeQuilometragem)
+                .NotNull()
+                .NotEmpty()
+                .GreaterThan(0);
+
+            RuleFor(x => x.PlanoKM_controlado_ValorDiario)
+                .NotNull()
+                .NotEmpty()
+                .GreaterThan(0);
+
+            RuleFor(x => x.PlanoKM_controlado_ValorPorKM)
+                .NotNull()
+                .NotEmpty()
+                .GreaterThan(0);
+
+
+
+        }
     }
 }
