dc548df077361b774861c127a43a9c2f97ce9c35
PedroSRN <pedraoflamenguista@hotmail.com>
2022-07-05 17:19:35 -0300

    Camada de Servico Plano de cobrança concluida

---
+++ Locadora-De-Veiculos.Aplicacao/ModuloPlanoDeCobranca/ServicoPlanoDeCobranca.cs
@@ -0,0 +1,63 @@
+﻿using FluentValidation.Results;
+using Locadora_De_Veiculos.Dominio.ModuloPlanosDeCobranca;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Locadora_De_Veiculos.Aplicacao.ModuloPlanoDeCobranca
+{
+    public class ServicoPlanoDeCobranca
+    {
+        private IRepositorioPlanoDeCobranca repositorioPlanoDeCobranca;
+
+        public ServicoPlanoDeCobranca(IRepositorioPlanoDeCobranca repositorioPlanoDeCobranca)
+        {
+            this.repositorioPlanoDeCobranca = repositorioPlanoDeCobranca;
+        }
+
+        public ValidationResult Inserir(PlanoDeCobranca planoDeCobranca)
+        {
+            ValidationResult resultadoValidacao = ValidarPlanoDeCobranca(planoDeCobranca);
+
+            if (resultadoValidacao.IsValid)
+                repositorioPlanoDeCobranca.Inserir(planoDeCobranca);
+            
+            return resultadoValidacao;
+        }
+
+
+        public ValidationResult Editar(PlanoDeCobranca planoDeCobranca)
+        {
+            ValidationResult resultadoValidacao = ValidarPlanoDeCobranca(planoDeCobranca);
+
+            if (resultadoValidacao.IsValid)
+                repositorioPlanoDeCobranca.Editar(planoDeCobranca);
+
+            return resultadoValidacao;
+        }
+
+        private ValidationResult ValidarPlanoDeCobranca(PlanoDeCobranca planoDeCobranca)
+        {
+            var validador = new ValidadorPlanoDeCobranca();
+
+            var resultadoValidacao = validador.Validate(planoDeCobranca);
+
+            if (IdCategoriaDuplicado(planoDeCobranca))
+                resultadoValidacao.Errors.Add(new ValidationFailure("Categoria", "Categoria duplicada"));
+
+            
+            return resultadoValidacao;
+        }
+
+        private bool IdCategoriaDuplicado(PlanoDeCobranca planoDeCobranca)
+        {
+            var planoEncontrado = repositorioPlanoDeCobranca.SelecionarIdCategoria(planoDeCobranca.CategoriaDeVeiculos.Id);
+
+            return planoEncontrado != null && 
+                   planoEncontrado.CategoriaDeVeiculos.Id == planoDeCobranca.CategoriaDeVeiculos.Id &&
+                   planoDeCobranca.Id != planoDeCobranca.Id;
+        }
+    }
+}
--- Locadora-De-Veiculos.Infra.Banco/ModuloPlanosDeCobranca/RepositorioPlanosDeCobrancaEmBancoDados.cs
+++ Locadora-De-Veiculos.Infra.Banco/ModuloPlanosDeCobranca/RepositorioPlanosDeCobrancaEmBancoDados.cs
@@ -2,6 +2,7 @@
 using Locadora_De_Veiculos.Infra.Banco.Compartilhado;
 using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -87,5 +88,30 @@ namespace Locadora_De_Veiculos.Infra.Banco.ModuloPlanosDeCobranca
                  TBPLANOCOBRANCA AS PLANO INNER JOIN TBCATEGORIAVEICULO AS CATEGORIA 
             ON 
                  PLANO.CATEGORIA_ID = CATEGORIA.ID";
+
+        protected  string sqlSelecionarPorIdCategoria =>
+            @"SELECT 
+                PLANO.ID,
+                PLANO.PLANODIARIO_VALORDIARIO,
+                PLANO.PLANODIARIO_VALORPORKM,
+                PLANO.PLANOKM_LIVRE_VALORDIARIO,
+                PLANO.PLANOKM_CONTROLADO_LIMITEDEQUILOMETRAGEM,
+                PLANO.PLANOKM_CONTROLADO_VALORDIARIO,
+                PLANO.PLANOKM_CONTROLADO_VALORPORKM,
+
+                CATEGORIA.ID AS ID_CATEGORIA,
+		        CATEGORIA.NOME AS NOME_CATEGORIA
+            FROM
+                 TBPLANOCOBRANCA AS PLANO INNER JOIN TBCATEGORIAVEICULO AS CATEGORIA 
+            ON 
+                 PLANO.CATEGORIA_ID = CATEGORIA.ID
+            WHERE
+	                   CATEGORIA.ID = CATEGORIA.ID;";
+
+
+        public PlanoDeCobranca SelecionarIdCategoria(int idCategoria)
+        {
+            return SelecionarPorParametro(sqlSelecionarPorIdCategoria, new SqlParameter("ID_CATEGORIA", idCategoria));
+        }
     }
 }
