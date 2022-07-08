cafa117677e51045211564be4af84fa20c2c8178
Carlos <cdgamerbr007@gmail.com>
2022-07-07 21:13:39 -0300

    Implementado log em Serviço veiculo

--- Locadora-De-Veiculos.Aplicacao/ModuloVeiculo/ServicoVeiculo.cs
+++ Locadora-De-Veiculos.Aplicacao/ModuloVeiculo/ServicoVeiculo.cs
@@ -21,21 +21,34 @@ namespace Locadora_De_Veiculos.Aplicacao.ModuloVeiculo
 
         public ValidationResult Inserir(Veiculo veiculo)
         {
+            logger.Information("Tentando inserir Veiculo... {@veiculo}", veiculo);
+
             ValidationResult resultadoValidacao = ValidarVeiculo(veiculo);
 
             if (resultadoValidacao.IsValid)
+            {
                 repositorioVeiculo.Inserir(veiculo);
-
+                logger.Information("Veiculo {@veiculo} inserido com sucesso.", veiculo.Id);
+            }
+            else
+                foreach (var erro in resultadoValidacao.Errors)
+                    logger.Warning("Falha ao tentar inserir Veiculo {VeiculoNome} com a placa {Placa} -> Motivo: {erro}", veiculo.Modelo, veiculo.Placa, erro.ErrorMessage);
             return resultadoValidacao;
         }
 
         public ValidationResult Editar(Veiculo veiculo)
         {
+            logger.Information("Tentando editar Veiculo... {@veiculo}", veiculo);
             ValidationResult resultadoValidacao = ValidarVeiculo(veiculo);
 
             if (resultadoValidacao.IsValid)
+            {
                 repositorioVeiculo.Editar(veiculo);
-
+                logger.Information("Veiculo {@veiculo} editado com sucesso.", veiculo.Id);
+            }
+            else
+                foreach (var erro in resultadoValidacao.Errors)
+                    logger.Warning("Falha ao tentar editar Veiculo {VeiculoNome} com a placa {Placa} -> Motivo: {erro}", veiculo.Modelo, veiculo.Placa, erro.ErrorMessage);
             return resultadoValidacao;
         }
 
