8f64eeb366742d83d14a11b14e984dca652cfd63
Carlos <cdgamerbr007@gmail.com>
2022-07-12 15:20:00 -0300

    Implementado part II refatoração Log

--- Locadora_De_Veiculos.Infra.Logging/ConfiguracaoLogsLocadora.cs
+++ Locadora_De_Veiculos.Infra.Logging/ConfiguracaoLogsLocadora.cs
@@ -1,5 +1,7 @@
-﻿using Serilog;
+﻿using Microsoft.Extensions.Configuration;
+using Serilog;
 using System;
+using System.IO;
 
 namespace Locadora_De_Veiculos.Infra.Logging
 {
@@ -7,9 +9,19 @@ namespace Locadora_De_Veiculos.Infra.Logging
     {
         public static void ConfigurarEscritaLog()
         {
+            var configuracao = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("ConfiguracaoAplicacao.json")
+                .Build();
+
+            var diretorioSaida = configuracao
+                .GetSection("ConfiguracaoLogs")
+                .GetSection("DiretorioSaida")
+                .Value;
+
             Log.Logger = new LoggerConfiguration()
                 .MinimumLevel.Debug()
-                .WriteTo.File("logs/log.txt", rollingInterval: RollingInterval.Day,
+                .WriteTo.File( diretorioSaida + "/log.txt", rollingInterval: RollingInterval.Day,
                 outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}")
                 .CreateLogger();
         }
