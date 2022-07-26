a4bfac0d6de0d2e5bb01ab5e8c72008c2929005d
TalesReigCordova <takesmohamed@gmail.com>
2022-07-26 10:21:49 -0300

    Pasta comparetilhada criada, erros para corrigir

---
+++ Locadora-De-Veiculos.Infra.Orm/LocaDoraDeVeiculosDbContext.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Locadora_De_Veiculos.Infra.Orm
+{
+    public class LocaDoraDeVeiculosDbContext : DbContext  //, IContextoPersistencia
+    {
+        private string connectionString;
+
+        public LocaDoraDeVeiculosDbContext(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public void GravarDados()
+        {
+            SaveChanges();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString);
+
+            ILoggerFactory loggerFactory = LoggerFactory.Create((x) =>
+            {
+                x.AddSerilog(Log.Logger);//instalar o pacote Serilog.Extensions.Logging
+            });
+
+            optionsBuilder.UseLoggerFactory(loggerFactory);
+
+            optionsBuilder.EnableSensitiveDataLogging();
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            var dllComConfiguracoesOrm = typeof(LocaDoraDeVeiculosDbContext).Assembly;
+
+            modelBuilder.ApplyConfigurationsFromAssembly(dllComConfiguracoesOrm);
+        }
+    }
+}
---
+++ Locadora-De-Veiculos.Infra.Orm/LocadoraDeVeiculosDbContextFactory.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace Locadora_De_Veiculos.Infra.Orm
+{
+    internal class LocadoraDeVeiculosDbContextFactory : IDesignTimeDbContextFactory<LocaDoraDeVeiculosDbContext>
+    {
+        public LocaDoraDeVeiculosDbContext CreateDbContext(string[] args)
+        {
+            var configuracao = new ConfigurationBuilder()
+             .SetBasePath(Directory.GetCurrentDirectory())
+             .AddJsonFile("ConfiguracaoAplicacao.json")
+             .Build();
+
+            var connectionString = configuracao.GetConnectionString("SqlServer");
+
+            return new LocaDoraDeVeiculosDbContext(connectionString);
+        }
+    }
+}
