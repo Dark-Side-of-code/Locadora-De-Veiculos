da824939a7e4dca012af0c60c8231e03c5a2f615
CarlosDanieldeAbreu <87880996+CarlosDanieldeAbreu@users.noreply.github.com>
2022-07-27 15:57:16 -0300

    Implementado refatoração funcionario orm

---
+++ Locadora-De-Veiculos.Infra.Orm/ModuloFuncionario/RepositorioFuncionarioOrm.cs
@@ -0,0 +1,33 @@
+﻿using Locadora_De_Veiculos.Dominio.ModuloFuncionario;
+using Locadora_De_Veiculos.Infra.Orm.Compartilhado;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Locadora_De_Veiculos.Infra.Orm.ModuloFuncionario
+{
+    public class RepositorioFuncionarioOrm : RepositorioBaseOrm<Funcionario>, IRepositorioFuncionario
+    {
+        private DbSet<Funcionario> funcionarios;
+        private readonly LocaDoraDeVeiculosDbContext dbContext;
+
+        public RepositorioFuncionarioOrm(LocaDoraDeVeiculosDbContext dbContext)
+        {
+            funcionarios = dbContext.Set<Funcionario>(); 
+            this.dbContext = dbContext;
+        }
+
+        public Funcionario SelecionarFuncionarioPorNome(string nome)
+        {
+            return funcionarios.SingleOrDefault(x => x.Nome == nome);
+        }
+
+        public Funcionario SelecionarFuncionarioPorUsuario(string usuario)
+        {
+            return funcionarios.SingleOrDefault(x => x.TipoFuncionario == usuario);
+        }
+    }
+}
